83fd75d1406bac725d44e978267fbded1272436b
learnudm01y <learnudm01y@gmail.com>
2025-11-26 02:32:38 +0200

    تحسين شامل لتصميم الموبايل - responsive محسن v10

--- force-update.js
+++ force-update.js
@@ -1,6 +1,6 @@
 // Force Update Script - Add timestamp to bypass cache
 (function() {
-    const VERSION = '9.0.0';
+    const VERSION = '10.0.0';
     const STORAGE_KEY = 'app-version';
     
     // Check stored version
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kabab-menu-v9';
+const CACHE_NAME = 'kabab-menu-v10';
 const urlsToCache = [
   '/',
   '/index.html',
